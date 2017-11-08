45cc923beccabfd1571691270eecbf048aa3576e
raj594 <rajaynes@gmail.com>
2017-11-08 01:02:20 -0600

    add logic

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,54 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create our routes and set up logic within those routes where required.
+
+router.post("/", function(req, res) {
+  console.log(req.body)
+  burger.create([
+    "burger_name"
+  ], [
+    req.body.burger_name
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var id = "id = " + req.params.id;
+
+  console.log("devoured burger", id);
+
+
+  burger.update({
+    devoured: true
+  }, id, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// router.delete("/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
+
+//   burger.delete(condition, function(result) {
+//     if (result.affectedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
+
+// Export routes for server.js to use.
+module.exports = router;
